a74ed638c4fbc465e322018583a7d371c4f4ea0a
Teo Bous <tbousiou@gmail.com>
2019-09-10 22:10:38 +0300

    Basic login logic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request
 from database import get_db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -18,11 +19,39 @@ def index():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		name = request.form['name']
+		password = request.form['password']
+		hashed_password = generate_password_hash(password, method='sha256')
+
+		db = get_db()
+		query = 'insert into users (name, password, expert, admin) values (?, ?, ?, ?)'
+		db.execute(query, [name, hashed_password, 0, 0])
+		db.commit()
+		return f'<h2>User {name} created with password {password}<h2>'
+
 	return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		name = request.form['name']
+		password = request.form['password']
+		#hashed_password = generate_password_hash(password, method='sha256')
+
+		db = get_db()
+		query = 'select id, name, password from users where name = ?'
+		user_login_cur = db.execute(query, [name])
+		user_result = user_login_cur.fetchone()
+		
+		if user_result and check_password_hash(user_result['password'], password):
+			return 'Success'
+		else:
+			return 'Fail'
+
+
+
 	return render_template('login.html')
 
 @app.route('/question')
